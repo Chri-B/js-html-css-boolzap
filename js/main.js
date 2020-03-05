b1cb94d9809baba176d922aa40b2a2b41e80919a
Christian Beretta <beretta.christian2602@gmail.com>
2020-03-05 17:08:13 +0100

    inizio 2.2

--- js/main.js
+++ js/main.js
@@ -6,6 +6,18 @@ $('.tasto-invio').click(function() {
     setTimeout(messageOk, 1000); // milestone 2.1
 });
 
+$('#find-contact').keyup(function(event) {
+    var carattereFiltro = $(this).val().toLowerCase(); // assegno alla variabile il valore del carattere inserito dall'utente e lo rendo minuscolo
+
+    $('.chat-preview h4').each(function() { // confronto il valore inserito dall'utente con OGNUNO degli elementi h4 (dove ci sono i nomi dei contatti)
+        if ($(this).text().toLowerCase().includes(carattereFiltro)) { // se il valore digitato dall'utente compare in uno dei miei contatti, allora lo mostro
+            $(this).show();
+        } else { // altrimenti lo scarto e quindi non lo visualizzo
+            $(this).hide();
+        }
+    });
+});
+
 $(document).keydown(function(event) {
     switch (event.which) {
         case 13:
@@ -15,7 +27,7 @@ $(document).keydown(function(event) {
     }
 });
 
-
+// funzione messaggio inviato dopo input utente
 function messageSent() {
     var messaggioInput = $('#messaggio-input').val();
     $('#messaggio-input').val('');
@@ -25,6 +37,7 @@ function messageSent() {
     $('.main-room').append(messaggio);
 };
 
+// funzione messaggi di risposta 'ok'
 function messageOk() {
     var messaggio = $('.template .chat-message').clone().addClass('received');
     messaggio.find('.testo-messaggio').text('ok');
@@ -32,9 +45,13 @@ function messageOk() {
     $('.main-room').append(messaggio);
 };
 
-
+// funzione per avere orario
 function getTime() {
     var dt = new Date();
     var time = dt.getHours() + ":" + dt.getMinutes();
     return time;
+};
+
+// funzione filtro contatti
+function filtroContatti() {
 };
