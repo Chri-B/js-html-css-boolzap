5912ae8db2b18916a9dc8706e410956cc77e3479
Christian Beretta <beretta.christian2602@gmail.com>
2020-03-05 16:14:07 +0100

    completamento milestone 1

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// console.log(time);
+
+
+$('.tasto-invio').click(function() {
+    messageSent();
+    setTimeout(messageOk, 1000);
+});
+
+$(document).keydown(function(event) {
+    switch (event.which) {
+        case 13:
+            messageSent();
+            setTimeout(messageOk, 1000);
+            break;
+    }
+});
+
+
+function messageSent() {
+    var messaggioInput = $('#messaggio-input').val();
+    $('#messaggio-input').val('');
+    var messaggio = $('.template .chat-message').clone().addClass('sent');
+    messaggio.find('.testo-messaggio').text(messaggioInput);
+    messaggio.children('.message-time').html(getTime());
+    $('.main-room').append(messaggio);
+};
+
+function messageOk() {
+    var messaggio = $('.template .chat-message').clone().addClass('received');
+    messaggio.find('.testo-messaggio').text('ok');
+    messaggio.children('.message-time').html(getTime());
+    $('.main-room').append(messaggio);
+}
+
+
+function getTime() {
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes();
+    return time;
+};
