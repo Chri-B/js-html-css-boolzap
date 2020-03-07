a6e295a4b86ff4d0948b7543f2a5ab5358cdaf81
Christian Beretta <beretta.christian2602@gmail.com>
2020-03-07 18:13:32 +0100

    modifiche

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ $('.chat-preview').click(function() {
 });
 
 // utilizzo del data per selezionare ogni chat container al click e mostrarlo
-$('.left .chat-list .chat-preview, .header-list .img-round img').click(function() { // al click della corrispondente chat-preview
+$('.left .chat-list .chat-preview').click(function() { // al click della corrispondente chat-preview
     var contatto = $(this).data('codiceContatto'); // associo alla variabile contatto il data-codice-contatto
     console.log(contatto);
     $('.right .chat-room-container').each(function() { // successivamente confronto i data-codice-contatto LEFT con quelli RIGHT
@@ -98,6 +98,14 @@ function getTime() {
     return time;
 };
 
+// funzione per scroll-down automatico
+// function scroll() { // dopo una certa qnt. di interazioni non scende più!
+//     var pixelScroll = $('.main-room').height();
+//     $('.main-room').scrollTop(pixelScroll);
+// };
+
+
+// FUNZIONE DA IMPLEMENTARE: FUNZIONA SOLO CON ID UNIVOCO! COME USARE SU OGNI CHAT???
 // funzione per scroll down automatico infinito
 function scroll() { // Funzione di autoscorrimento in basso
     var scrollBot = document.getElementById('scrolled');
